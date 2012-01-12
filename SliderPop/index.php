17730f15b7d13c9d1900e2dc8e6d736858aed3a1
angelo <ngelmat@yahoo.com>
2012-01-12 02:46:17 -0800

    update index Slide-Pop for Image Gallery

--- SliderPop/index.php
+++ SliderPop/index.php
@@ -1,7 +1,6 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
 <head>
-    <head>
         <title>Slider-Pop Image Gallery</title>
 		<meta charset="UTF-8" />
 		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"> 
